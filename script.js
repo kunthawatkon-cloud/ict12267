96ac3ab8d3c7adaca6d58c37d1f9dec267aedfa8
kunthawatkon-cloud <kunthawat.kon@spumail.net>
2026-03-10 10:18:05 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function searchMovie() {
-    let keyword = document.getElementById("searchInput").value;
-
-    if(keyword.trim() === "") {
-        alert("กรุณาพิมพ์คำค้นหา");
-    } else {
-        alert("กำลังค้นหา: " + keyword);
-    }
-}
